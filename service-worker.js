91e2deb0cdc0377697a97f027c522956ca37bddd
T1-arc <engr.taha.ali1@outlook.com>
2026-09-01 02:21:44 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "vibe-ledger-v1";
+const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// App shell: cache-first. Everything else (Firebase, CDN libs): network-first, falling back to cache.
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  const isShell = APP_SHELL.some((p) => url.pathname.endsWith(p.replace("./", "")));
+
+  if (isShell) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request)
+        .then((res) => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
+});
