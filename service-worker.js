127ad223c400b44eeaef06731815401347760912
T1-arc <engr.taha.ali1@outlook.com>
2026-09-01 14:14:11 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vibe-ledger-v2";
+const CACHE_NAME = "vibe-ledger-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
